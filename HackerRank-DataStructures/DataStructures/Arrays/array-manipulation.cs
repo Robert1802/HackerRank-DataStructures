ac52c5a6e637aec622b523325999d1a376d9931c
Roberto Moura <roblm_@hotmail.com>
2025-10-22 10:11:36 -0300

    array manipulation

---
+++ HackerRank-DataStructures/DataStructures/Arrays/array-manipulation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank_DataStructures.DataStructures.Arrays
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'arrayManipulation' function below.
+         *
+         * The function is expected to return a LONG_INTEGER.
+         * The function accepts following parameters:
+         *  1. INTEGER n
+         *  2. 2D_INTEGER_ARRAY queries
+         */
+
+        public static long arrayManipulation(int n, List<List<int>> queries)
+        {
+            long[] arr = new long[n + 2];
+
+            foreach (var query in queries)
+            {
+                int a = query[0];
+                int b = query[1];
+                int k = query[2];
+
+                arr[a] += k;
+                arr[b + 1] -= k;
+            }
+
+            long max = 0;
+            long current = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                current += arr[i];
+                if (current > max)
+                {
+                    max = current;
+                }
+            }
+
+            return max;
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -11,6 +11,7 @@ I will leave the link to each challenge and my code on this page.<br/>
 - Dynamic Array - [HackerRank](https://www.hackerrank.com/challenges/dynamic-array/problem) - [Solution](https://github.com/Robert1802/HackerRank-DataStructures/blob/master/HackerRank-DataStructures/DataStructures/Arrays/dynamic-array.cs)
 - Left Rotation - [HackerRank](https://www.hackerrank.com/challenges/array-left-rotation/problem) - [Solution](https://github.com/Robert1802/HackerRank-DataStructures/blob/master/HackerRank-DataStructures/DataStructures/Arrays/array-left-rotation.cs)
 - Sparse Arrays - [HackerRank](https://www.hackerrank.com/challenges/sparse-arrays/problem) - [Solution](https://github.com/Robert1802/HackerRank-DataStructures/blob/master/HackerRank-DataStructures/DataStructures/Arrays/sparse-arrays.cs)
+- Array Manipulation - [HackerRank](https://www.hackerrank.com/challenges/crush/problem) - [Solution](https://github.com/Robert1802/HackerRank-DataStructures/blob/master/HackerRank-DataStructures/DataStructures/Arrays/array-manipulation.cs)
 
 ## Linked Lists
 
